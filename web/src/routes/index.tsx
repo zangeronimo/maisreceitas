2355295f00fbd5c5f0c6e5e482200c51063f4b04
Luciano Zangeronimo <zangeronimo@gmail.com>
2021-01-27 07:58:38 -0300

    Initial commit

--- web/src/routes/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import AppRoutes from './app.routes';
-
-const Routes: React.FC = () => <AppRoutes />
-
-export default Routes;
--- web/src/services/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'https://webeditorapi.tudolinux.com.br',
-});
-
-export default api;
