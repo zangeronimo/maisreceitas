ab7daf09b8859e4a9696f8e2d1f06048961b9be4
Luciano Zangeronimo <zangeronimo@gmail.com>
2021-01-30 11:45:41 -0300

    adicionado ads 250 x 250 no sidebar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,11 @@ import GlobalStyles from '@/styles/GlobalStyles';
 import { Router } from 'next/router';
 import * as gtag from '../lib/gtag'
 import * as ads from '../lib/ads';
+import { useEffect } from 'react';
+
+useEffect(() => {
+  ads.load();
+}, []);
 
 const routeChanged = (url: any) => {
   gtag.pageview(url);
