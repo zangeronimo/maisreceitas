d732ef8f1c47aa89c51aba8c1abe1cc16ae6e131
Luciano Zangeronimo <zangeronimo@gmail.com>
2021-01-30 12:15:59 -0300

    add react-adsense-google

--- src/components/Adsense250x250/index.tsx
+++ src/components/Adsense250x250/index.tsx
@@ -7,7 +7,7 @@ export default function Adsense250x250() {
       <Adsense
         client="ca-pub-0338836461603030"
         slot="7873909957"
-        style={{ width: 500, height: 300 }}
+        style={{ width: 250, height: 250 }}
         format=""
       />
     </Container>
