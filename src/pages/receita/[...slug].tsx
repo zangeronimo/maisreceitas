62f312ab6248030f3936b916fcf221fc476862d1
Zangeronimo <zangeronimo@gmail.com>
2023-01-07 15:33:35 -0300

    fix: fix useRoute error

--- src/pages/receita/[...slug].tsx
+++ src/pages/receita/[...slug].tsx
@@ -1,16 +1,18 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Recipe() {
   const router = useRouter();
-
   const slug = router.asPath.split("/").pop();
 
-  if (!slug.includes(".htm")) {
-    if (slug != "") router.push(`/busca/${slug.replaceAll("-", "%20")}`);
-    else router.push("/404");
-  } else {
-    router.push(`/receita/${slug.replace(".htm", "")}`);
-  }
+  useEffect(() => {
+    if (!slug.includes(".htm")) {
+      if (slug != "") router.push(`/busca/${slug.replaceAll("-", "%20")}`);
+      else router.push("/404");
+    } else {
+      router.push(`/receita/${slug.replace(".htm", "")}`);
+    }
+  }, [slug]);
 
   return <p>Carregando...</p>;
 }
