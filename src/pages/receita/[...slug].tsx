2df9e89dd5162001865dd3f7889de980ca6d9cf3
Zangeronimo <zangeronimo@gmail.com>
2023-01-07 15:50:30 -0300

    refactor: simplify the not found recipe to redirect

--- src/pages/receita/[...slug].tsx
+++ src/pages/receita/[...slug].tsx
@@ -3,11 +3,22 @@ import { useEffect } from "react";
 
 export default function Recipe() {
   const router = useRouter();
-  const slug = router.asPath.split("/").pop();
+  const slug = router.asPath
+    .split("/")
+    .pop()
+    .replaceAll("---", "-")
+    .replaceAll("--", "-");
 
   useEffect(() => {
-    if (slug) router.push(`/busca/${slug.replaceAll("-", "%20")}`);
-    else router.push("/404");
+    if (!slug.includes(".htm")) {
+      if (slug != "") {
+        router.push(`/busca/${slug.replaceAll("-", "%20")}`);
+      } else {
+        router.push("/404");
+      }
+    } else {
+      router.push(`/receita/${slug.replace("-.htm", "").replace(".htm", "")}`);
+    }
   }, [slug]);
 
   return <p>Carregando...</p>;
