1022fe6256144a56ebc5314edc9ff14da54e03e9
Zangeronimo <zangeronimo@gmail.com>
2023-01-08 08:38:13 -0300

    fix: change inactive course from marketing

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,33 +1,44 @@
-import Image from 'next/image'
-import FbPage from '../FbPage';
-import { Container, Content } from './styles';
+import Image from "next/image";
+import FbPage from "../FbPage";
+import { Container, Content } from "./styles";
 
-export const Aside:React.FC = () => {
+export const Aside: React.FC = () => {
   return (
     <Container>
       <Content>
-
-        <a href="https://sun.eduzz.com/1062375?a=24789011" target="_blank" title="Panetones e Chocotones - Gourmet">
+        <a
+          href="https://sun.eduzz.com/1062375?a=24789011"
+          target="_blank"
+          title="Panetones e Chocotones - Gourmet"
+        >
           <Image
-            src='/eduzz/chocotone_panetone_gourmet.png'
+            src="/eduzz/chocotone_panetone_gourmet.png"
             alt="Panetones e Chocotones - Gourmet"
             width={280}
             height={130}
           />
         </a>
 
-        <a href="https://sun.eduzz.com/681241?a=24789011" target="_blank" title="Receitas de Amigurumi">
+        <a
+          href="https://sun.eduzz.com/1081056?a=24789011"
+          target="_blank"
+          title="Resina'Mour - Artesanato em Resina"
+        >
           <Image
-            src='/eduzz/receitas_amigurumi.png'
-            alt="Receitas de Amigurumi"
+            src="/eduzz/curso_artesanato.png"
+            alt="Resina'Mour - Artesanato em Resina"
             width={280}
             height={130}
           />
         </a>
 
-        <a href="https://sun.eduzz.com/730181?a=24789011" target="_blank" title="Close Friends Shark">
+        <a
+          href="https://sun.eduzz.com/730181?a=24789011"
+          target="_blank"
+          title="Close Friends Shark"
+        >
           <Image
-            src='/eduzz/close_friends_shark.png'
+            src="/eduzz/close_friends_shark.png"
             alt="Close Friends Shark"
             width={280}
             height={130}
@@ -44,5 +55,5 @@ export const Aside:React.FC = () => {
         />
       </Content>
     </Container>
-  )
-}
+  );
+};
